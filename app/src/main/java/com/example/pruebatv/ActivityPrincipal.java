862134634b66b5bcbaf9ee0aa157bc69bd071fa2
Ra�aul Herrera Macias <facelessdivinegmail.com>
2022-08-05 20:28:46 -0600

    first commit

---
+++ app/src/main/java/com/example/pruebatv/ActivityPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.pruebatv;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityPrincipal extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ app/src/main/java/com/example/pruebatv/FragmentPrincipal.java
@@ -0,0 +1,27 @@
+package com.example.pruebatv;
+
+import android.os.Bundle;
+
+import androidx.leanback.app.BrowseSupportFragment;
+
+import com.google.gson.Gson;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class FragmentPrincipal extends BrowseSupportFragment {
+    //@Override
+    //public void onActivityCreated(Bundle savedInstanceState){
+       // leerDatos();
+        //iniciarInterfazUsuario();
+        //cargarListas();
+
+    //}
+    //private void leerDatos(){
+        //MovieList.list = new ArrayList<Movie>();
+        //String json = Utils.loadJSONFromResource( getActivity(), R.raw.movies);
+        //Gson gson = new Gson();
+        //Type collection = new TypeToken<ArrayList<Movie>>(){}.getType();
+        //MovieList.list = gson.fromJson(json, collection);
+    //}
+}
